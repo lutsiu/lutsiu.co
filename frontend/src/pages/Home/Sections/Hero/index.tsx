8f6bc6e2eddf078bfa90fac93da663e02bac7afa
lutsiu <oleksandr.lutsiuk.1603@gmail.com>
2023-11-02 10:19:08 +0100

    bug was fixed

--- frontend/src/pages/Home/Sections/Hero/index.tsx
+++ frontend/src/pages/Home/Sections/Hero/index.tsx
@@ -1,18 +1,15 @@
-import useGetScreenWidth from "../../../../hooks/useGetScreenWidth";
 import shapeFuture from "../../../../assets/home-page/hero/shape-future.png";
 export default function HeroSection() {
-  const screenWidth = useGetScreenWidth();
+
   return (
     <>
-      {screenWidth >= 476 && (
-        <section className="h-screen section-hero">
-          <img
-            src={shapeFuture}
-            alt="shape-future"
-            className="w-full  object-cover"
-          />
-        </section>
-      )}
+      <section className="h-screen section-hero hidden sm:block">
+        <img
+          src={shapeFuture}
+          alt="shape-future"
+          className="w-full  object-cover"
+        />
+      </section>
     </>
   );
 }
